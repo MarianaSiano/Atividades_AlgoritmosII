a63fc182f30bc047be5736b79f213125c686b132
MarianaSiano <marianasianop@gmail.com>
2025-04-30 18:56:58 -0300

    Penultimo exercicio feito com sucesso

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ run2:
 	./main
 
 # Defina os arquivos que você deseja limpar
-FILES_TO_CLEAN = main *.dot *.png *.txt *.exe
+FILES_TO_CLEAN = main
 
 # Alvo para limpar arquivos
 clear:
--- aula_revisao.cpp
+++ aula_revisao.cpp
@@ -82,92 +82,130 @@ unsigned long long fatorial(int n)
     return n * fatorial(n - 1); //Chamada recursiva
 }
 
-int main()
+//Atividade 12
+float maiorValorPar(int n, float vetor[])
 {
-    cout << "Ativide 1 - Parte 1" <<endl;
-    cout << endl;
+    float maior = 0.0;
 
-    float media = 0.0;
-    int tamanho = 0;
-
-    cout << "Digite um numero SEM VIRGULA, para a quantidade de numeros: " << endl;
-    cin >> tamanho;
-
-    media = calculaMedia(tamanho);
-    cout << "Tamanho: " << tamanho << endl;
-    cout << "Media: " << media << endl;
-    cout << endl;
-
-    cout << "Parte 2" << endl;
-    cout << endl;
-
-    float vetor[tamanho];
-
-    for(int i = 0; i < tamanho; i++) {
-        cout << "Digite o valor (parte 2): " << endl;
-        cin >> vetor[i];
+    for(int i = 0; i < n; i++) {
+        if(vetor[i] > maior && (int)vetor[i] % 2 == 0)
+            maior = vetor[i];
     }
 
-    cout << "Numeros digitados: ";
-    for(int i = 0; i < tamanho; i++)
-        cout << " " <<vetor[i] << " ";
-    cout << endl;
+    return maior;
+}
 
-    float mediaVet = vetorCalculaMedia(tamanho, vetor);
-    cout << "Tamanho: " << tamanho << endl;
-    cout << "Media: " << media << endl;
-    cout << endl;
+int main()
+{
+    // cout << "Ativide 1 - Parte 1" <<endl;
+    // cout << endl;
 
-    cout << "Atividade 4" << endl;
-    cout << endl;
+    // float media = 0.0;
+    // int tamanho = 0;
 
-    string str;
-    char ch;
+    // cout << "Digite um numero SEM VIRGULA, para a quantidade de numeros: " << endl;
+    // cin >> tamanho;
 
-    cout << "Digite uma string: " << endl;
-    cin.ignore(); //Limpa o buffer de entrada
-    getline(cin, str);
+    // media = calculaMedia(tamanho);
+    // cout << "Tamanho: " << tamanho << endl;
+    // cout << "Media: " << media << endl;
+    // cout << endl;
 
-    cout << "Digite um caractere: " << endl;
-    cin >> ch;
-    cin.ignore(); //Limpa o buffer de entrada
+    // cout << "Parte 2" << endl;
+    // cout << endl;
 
-    int posicao = 0;
-    posicao = procuraChar(str, ch);
+    // float vetor[tamanho];
 
-    if(posicao != -1)
-        cout << "O caractere " << ch << " foi encontrado na posicao: " << posicao << endl;
-    else
-        cout << "O caractere " << ch << " NAO foi encontrado na string." << endl;
-    cout << endl;
+    // for(int i = 0; i < tamanho; i++) {
+    //     cout << "Digite o valor (parte 2): " << endl;
+    //     cin >> vetor[i];
+    // }
 
-    cout << endl;
-    cout << "Atividade 5" << endl;
-    cout << endl;
+    // cout << "Numeros digitados: ";
+    // for(int i = 0; i < tamanho; i++)
+    //     cout << " " <<vetor[i] << " ";
+    // cout << endl;
 
-    int numero;
-    numero = 0;
+    // float mediaVet = vetorCalculaMedia(tamanho, vetor);
+    // cout << "Tamanho: " << tamanho << endl;
+    // cout << "Media: " << media << endl;
+    // cout << endl;
 
-    cout << "Digite um numero: " << endl;
-    cin >> numero;
+    // cout << "Atividade 4" << endl;
+    // cout << endl;
+
+    // string str;
+    // char ch;
+
+    // cout << "Digite uma string: " << endl;
+    // cin.ignore(); //Limpa o buffer de entrada
+    // getline(cin, str);
+
+    // cout << "Digite um caractere: " << endl;
+    // cin >> ch;
+    // cin.ignore(); //Limpa o buffer de entrada
+
+    // int posicao = 0;
+    // posicao = procuraChar(str, ch);
+
+    // if(posicao != -1)
+    //     cout << "O caractere " << ch << " foi encontrado na posicao: " << posicao << endl;
+    // else
+    //     cout << "O caractere " << ch << " NAO foi encontrado na string." << endl;
+    // cout << endl;
+
+    // cout << endl;
+    // cout << "Atividade 5" << endl;
+    // cout << endl;
+
+    // int numero;
+    // numero = 0;
+
+    // cout << "Digite um numero: " << endl;
+    // cin >> numero;
     
-    if(ehPrimo(numero))
-        cout << "O numero " << numero << " eh primo." << endl;
-    else
-        cout << "O numero " << numero << " NAO eh primo." << endl;
+    // if(ehPrimo(numero))
+    //     cout << "O numero " << numero << " eh primo." << endl;
+    // else
+    //     cout << "O numero " << numero << " NAO eh primo." << endl;
 
+    // cout << endl;
+    // cout << "Atividade 7" << endl;
+    // cout << endl;
+
+    // int numFatorial;
+    // numFatorial = 1;
+
+    // cout << "Digite um numero para calcular o fatorial: " << endl;
+    // cin >> numFatorial;
+
+    // unsigned long long resultadoFatorial = fatorial(numFatorial);
+    // cout << "O fatorial de " << numFatorial << " eh: " << resultadoFatorial << endl;
     cout << endl;
-    cout << "Atividade 7" << endl;
+
+    cout << "Atividade 12" << endl;
     cout << endl;
 
-    int numFatorial;
-    numFatorial = 1;
+    int n;
+    n = 0;
 
-    cout << "Digite um numero para calcular o fatorial: " << endl;
-    cin >> numFatorial;
+    cout << "Digite um numero SEM VIRGULA, para a quantidade de numeros: " << endl;
+    cin >> n;
 
-    unsigned long long resultadoFatorial = fatorial(numFatorial);
-    cout << "O fatorial de " << numFatorial << " eh: " << resultadoFatorial << endl;
+    float vet[n];
+    for(int i = 0; i < n; i++) {
+        cout << "Digite o valor: " << endl;
+        cin >> vet[i];
+    }
+    cout << endl;
+
+    cout << "Numeros digitados: ";
+    for(int i = 0; i < n; i++)
+        cout << " " << vet[i];
+    cout << endl;
+
+    float maiorPar = maiorValorPar(n, vet);
+    cout << "O maior valor par eh: " << maiorPar << endl;
 
     return 0;
 }
