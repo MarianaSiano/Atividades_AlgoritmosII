78f1351279c66b279d281bd70dcaf63a6c8b7b56
MarianaSiano <marianasianop@gmail.com>
2025-04-30 17:28:57 -0300

    .gitignore feito e atividade do fatorial feito e funcionando perfeitamente

--- aula_revisao.cpp
+++ aula_revisao.cpp
@@ -73,64 +73,73 @@ bool ehPrimo(int n)
     return true; //Se não encontrou divisores, é primo
 }
 
+//Atividade 7
+unsigned long long fatorial(int n)
+{
+    if(n == 1) 
+        return 1; //Fatorial de 1 é 1
+
+    return n * fatorial(n - 1); //Chamada recursiva
+}
+
 int main()
 {
-    // cout << "Ativide 1 - Parte 1" <<endl;
-    // cout << endl;
+    cout << "Ativide 1 - Parte 1" <<endl;
+    cout << endl;
 
-    // float media = 0.0;
-    // int tamanho = 0;
+    float media = 0.0;
+    int tamanho = 0;
 
-    // cout << "Digite um numero SEM VIRGULA, para a quantidade de numeros: " << endl;
-    // cin >> tamanho;
+    cout << "Digite um numero SEM VIRGULA, para a quantidade de numeros: " << endl;
+    cin >> tamanho;
 
-    // media = calculaMedia(tamanho);
-    // cout << "Tamanho: " << tamanho << endl;
-    // cout << "Media: " << media << endl;
-    // cout << endl;
+    media = calculaMedia(tamanho);
+    cout << "Tamanho: " << tamanho << endl;
+    cout << "Media: " << media << endl;
+    cout << endl;
 
-    // cout << "Parte 2" << endl;
-    // cout << endl;
+    cout << "Parte 2" << endl;
+    cout << endl;
 
-    // float vetor[tamanho];
+    float vetor[tamanho];
 
-    // for(int i = 0; i < tamanho; i++) {
-    //     cout << "Digite o valor (parte 2): " << endl;
-    //     cin >> vetor[i];
-    // }
+    for(int i = 0; i < tamanho; i++) {
+        cout << "Digite o valor (parte 2): " << endl;
+        cin >> vetor[i];
+    }
 
-    // cout << "Numeros digitados: ";
-    // for(int i = 0; i < tamanho; i++)
-    //     cout << " " <<vetor[i] << " ";
-    // cout << endl;
+    cout << "Numeros digitados: ";
+    for(int i = 0; i < tamanho; i++)
+        cout << " " <<vetor[i] << " ";
+    cout << endl;
 
-    // float mediaVet = vetorCalculaMedia(tamanho, vetor);
-    // cout << "Tamanho: " << tamanho << endl;
-    // cout << "Media: " << media << endl;
-    // cout << endl;
+    float mediaVet = vetorCalculaMedia(tamanho, vetor);
+    cout << "Tamanho: " << tamanho << endl;
+    cout << "Media: " << media << endl;
+    cout << endl;
 
-    // cout << "Atividade 4" << endl;
-    // cout << endl;
+    cout << "Atividade 4" << endl;
+    cout << endl;
 
-    // string str;
-    // char ch;
+    string str;
+    char ch;
 
-    // cout << "Digite uma string: " << endl;
-    // cin.ignore(); //Limpa o buffer de entrada
-    // getline(cin, str);
+    cout << "Digite uma string: " << endl;
+    cin.ignore(); //Limpa o buffer de entrada
+    getline(cin, str);
 
-    // cout << "Digite um caractere: " << endl;
-    // cin >> ch;
-    // cin.ignore(); //Limpa o buffer de entrada
+    cout << "Digite um caractere: " << endl;
+    cin >> ch;
+    cin.ignore(); //Limpa o buffer de entrada
 
-    // int posicao = 0;
-    // posicao = procuraChar(str, ch);
+    int posicao = 0;
+    posicao = procuraChar(str, ch);
 
-    // if(posicao != -1)
-    //     cout << "O caractere " << ch << " foi encontrado na posicao: " << posicao << endl;
-    // else
-    //     cout << "O caractere " << ch << " NAO foi encontrado na string." << endl;
-    // cout << endl;
+    if(posicao != -1)
+        cout << "O caractere " << ch << " foi encontrado na posicao: " << posicao << endl;
+    else
+        cout << "O caractere " << ch << " NAO foi encontrado na string." << endl;
+    cout << endl;
 
     cout << endl;
     cout << "Atividade 5" << endl;
@@ -146,6 +155,19 @@ int main()
         cout << "O numero " << numero << " eh primo." << endl;
     else
         cout << "O numero " << numero << " NAO eh primo." << endl;
+
+    cout << endl;
+    cout << "Atividade 7" << endl;
+    cout << endl;
+
+    int numFatorial;
+    numFatorial = 1;
+
+    cout << "Digite um numero para calcular o fatorial: " << endl;
+    cin >> numFatorial;
+
+    unsigned long long resultadoFatorial = fatorial(numFatorial);
+    cout << "O fatorial de " << numFatorial << " eh: " << resultadoFatorial << endl;
 
     return 0;
 }
