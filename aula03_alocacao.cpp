8d10a047599bc15593e46981e16f4cab66993e5c
MarianaSiano <marianasianop@gmail.com>
2025-05-12 14:06:10 -0300

    Leitura de numeros inteiros no vetor dando certo

--- aula03_alocacao.cpp
+++ aula03_alocacao.cpp
@@ -4,5 +4,19 @@ using namespace std;
 
 int main()
 {
+    cout << "Atividade 1" << endl;
+    cout << endl;
+
+    int n;
+    cout << "Digite um numero INTEIRO => ";
+    cin >> n;
+
+    int *vetor = new int [n];
+
+    for(int i = 0; i < n; i++){
+        cout << "Digite os valores INTEIROS que deseja calcular a media => ";
+        cin >> vetor[i];
+    }
+
     return 0;
 }
