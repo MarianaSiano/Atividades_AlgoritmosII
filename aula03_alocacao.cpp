db4a47ee2c27df434fd7bd9a12920e3ff75a3a66
MarianaSiano <marianasianop@gmail.com>
2025-05-12 14:18:08 -0300

    Atividade 1 concluida com sucesso

--- aula03_alocacao.cpp
+++ aula03_alocacao.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    //Atividade 1
     cout << "Atividade 1" << endl;
     cout << endl;
 
@@ -11,12 +12,37 @@ int main()
     cout << "Digite um numero INTEIRO => ";
     cin >> n;
 
+    cout << endl;
+
+    cout << "Alocando memoria com o numero desejado" << endl;
+    cout << endl;
+
     int *vetor = new int [n];
 
     for(int i = 0; i < n; i++){
         cout << "Digite os valores INTEIROS que deseja calcular a media => ";
         cin >> vetor[i];
     }
+
+    int soma;
+    soma = 0;
+
+    cout << "Somando os valores do vetor" << endl;
+    for(int i = 0; i < n; i++)
+        soma += vetor[i];
+    
+    cout << "Total da soma => " << soma << endl;
+    cout << endl;
+
+    int media = 0;
+
+    media = soma / n;
+
+    cout << "A media calculada foi de => " << media << endl;
+    cout << endl;
+
+    cout << "Desalocando a memoria" << endl;
+    delete [] vetor;
 
     return 0;
 }
