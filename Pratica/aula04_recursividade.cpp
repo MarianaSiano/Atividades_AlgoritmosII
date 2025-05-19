3b95fb20b52a97208efa1b94ab71e20638a44067
MarianaSiano <marianasianop@gmail.com>
2025-05-19 09:20:06 -0300

    Atividade 1 - Fatorial feita

--- Pratica/aula04_recursividade.cpp
+++ Pratica/aula04_recursividade.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
+unsigned long long fatorial(int n)
+{
+    if(n == 0 || n == 1)
+        return 1;
+    else
+        return n * fatorial(n - 1); //Chamada recursiva
+}
+
 int main ()
 {
+    cout << "================== Atividade 1 ==================" << endl;
+    cout << endl;
+
+    int numero;
+    cout << "Digite um numero INTEIRO => ";
+    cin >> numero;
+
+    cout << "Calculando o faltorial de " << numero << endl;
+    unsigned long long resultadoFatorial = 1;
+    resultadoFatorial = fatorial(numero);
+    cout << endl;
+    cout << "Resultado do calculo desse fatorial => " << resultadoFatorial << endl;
+    cout << endl;
+
     return 0;
 }
