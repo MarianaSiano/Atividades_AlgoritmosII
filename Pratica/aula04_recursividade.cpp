2c919421db31ae4dfe0d64feeba3c84203f93bf6
MarianaSiano <marianasianop@gmail.com>
2025-05-19 09:40:52 -0300

    Atividade - Imprime Intervalo feito e concluido

--- Pratica/aula04_recursividade.cpp
+++ Pratica/aula04_recursividade.cpp
@@ -10,20 +10,43 @@ unsigned long long fatorial(int n)
         return n * fatorial(n - 1); //Chamada recursiva
 }
 
+void imprimeIntervalo(int a, int b, int inc)
+{
+    if((inc > 0 && a > b) || (inc < 0 && a < b))
+        return; //Condição de parada
+    cout << a << " ";
+    imprimeIntervalo(a + inc, b, inc); //Chamada recursiva
+}
+
 int main ()
 {
-    cout << "================== Atividade 1 ==================" << endl;
+    // cout << "================== Atividade 1 ==================" << endl;
+    // cout << endl;
+
+    // int numero;
+    // cout << "Digite um numero INTEIRO => ";
+    // cin >> numero;
+
+    // cout << "Calculando o faltorial de " << numero << endl;
+    // unsigned long long resultadoFatorial = 1;
+    // resultadoFatorial = fatorial(numero);
+    // cout << endl;
+    // cout << "Resultado do calculo desse fatorial => " << resultadoFatorial << endl;
     cout << endl;
 
-    int numero;
-    cout << "Digite um numero INTEIRO => ";
-    cin >> numero;
-
-    cout << "Calculando o faltorial de " << numero << endl;
-    unsigned long long resultadoFatorial = 1;
-    resultadoFatorial = fatorial(numero);
+    cout << "================== Atividade 2 ==================" << endl;
     cout << endl;
-    cout << "Resultado do calculo desse fatorial => " << resultadoFatorial << endl;
+
+    int inicio, final, incremento;
+    cout << "Digite um numero INTEIRO de inicio => ";
+    cin >> inicio;
+    cout << "Digite um numero INTEIRO para o final => ";
+    cin >> final;
+    cout << "Digite o incremente desejado (NUMERO INTEIRO) => ";
+    cin >> incremento;
+    cout << endl;
+
+    imprimeIntervalo(inicio, final, incremento);
     cout << endl;
 
     return 0;
