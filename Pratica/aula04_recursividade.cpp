baed3c93c894c6698293b6a5867a814968c04bbe
MarianaSiano <marianasianop@gmail.com>
2025-05-19 09:53:04 -0300

    Atividade 3 - Imprimir Decrescente terminada e funcionando

--- Pratica/aula04_recursividade.cpp
+++ Pratica/aula04_recursividade.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//Atividade 1
 unsigned long long fatorial(int n)
 {
     if(n == 0 || n == 1)
@@ -10,6 +11,7 @@ unsigned long long fatorial(int n)
         return n * fatorial(n - 1); //Chamada recursiva
 }
 
+//Atividade 2
 void imprimeIntervalo(int a, int b, int inc)
 {
     if((inc > 0 && a > b) || (inc < 0 && a < b))
@@ -18,20 +20,29 @@ void imprimeIntervalo(int a, int b, int inc)
     imprimeIntervalo(a + inc, b, inc); //Chamada recursiva
 }
 
+//Atividade 3
+void imprimeDecrescente(int n)
+{
+    if(n < 0)
+        return; //Condição de parada
+    cout << n << " ";
+    imprimeDecrescente(n - 1); //Chamada recursiva com n - 1
+}
+
 int main ()
 {
-    // cout << "================== Atividade 1 ==================" << endl;
-    // cout << endl;
+    cout << "================== Atividade 1 ==================" << endl;
+    cout << endl;
 
-    // int numero;
-    // cout << "Digite um numero INTEIRO => ";
-    // cin >> numero;
+    int numero;
+    cout << "Digite um numero INTEIRO => ";
+    cin >> numero;
 
-    // cout << "Calculando o faltorial de " << numero << endl;
-    // unsigned long long resultadoFatorial = 1;
-    // resultadoFatorial = fatorial(numero);
-    // cout << endl;
-    // cout << "Resultado do calculo desse fatorial => " << resultadoFatorial << endl;
+    cout << "Calculando o faltorial de " << numero << endl;
+    unsigned long long resultadoFatorial = 1;
+    resultadoFatorial = fatorial(numero);
+    cout << endl;
+    cout << "Resultado do calculo desse fatorial => " << resultadoFatorial << endl;
     cout << endl;
 
     cout << "================== Atividade 2 ==================" << endl;
@@ -46,7 +57,21 @@ int main ()
     cin >> incremento;
     cout << endl;
 
+    cout << "Impriminto em intervalos" << endl;
     imprimeIntervalo(inicio, final, incremento);
+    cout << endl;
+
+    cout << "================== Atividade 3 ==================" << endl;
+    cout << endl;
+
+    int valor;
+
+    cout << "Digite um numero INTEIRO => ";
+    cin >> valor;
+    cout << endl;
+
+    cout << "Imprimindo em decrescente" << endl;
+    imprimeDecrescente(valor);
     cout << endl;
 
     return 0;
