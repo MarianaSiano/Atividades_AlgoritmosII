49c54042784322bc9e5ec9169fbd562246b04aa3
MarianaSiano <marianasianop@gmail.com>
2025-05-21 15:02:40 -0300

    atividade 4 feita

--- Teorica/aula01-parte02_alocacao.cpp
+++ Teorica/aula01-parte02_alocacao.cpp
@@ -29,6 +29,7 @@ int main()
         cout << "1. Media dos Valores" << endl;
         cout << "2. Media dos Valores com Funcao" << endl;
         cout << "3. Alocando Vetor Separadamente e Calculando a Media" << endl;
+        cout << "4. Ponteiros e Alocacao Dinamica" << endl;
         cout << "0. Sair" << endl;
         cout << "Escolha uma opcao => ";
         cin >> opcao;
@@ -146,6 +147,54 @@ int main()
                 //Liberacao de memoria
                 delete [] vetor;
                 vetor = nullptr; //Boa pratica após liberar memoria
+                break;
+            }
+
+            case 4: {
+                //Declaracoes e inicializacoes
+                int a, b, c;
+                int *v, *px, *py, *pz;
+                a = 1;
+                b = 2;
+                c = 3;
+
+                //Passo 1: Faça px e py apontarem, respectivamente, para a e b
+                px = &a;
+                py = &b;
+
+                //Passo 2: Incremente o valor de a
+                (*px)++; //Ou a++
+
+                //Passo 3: Atribua o valor de c ao local apontado por py
+                *py = c;
+
+                //Passo 4: Aloque um vetor de 3 elementos inteiros e armazene o endereço resultante em v
+                v = new int[3];
+
+                //Passo 5: Preencha a última posição do vetor com o valor 10
+                v[2] = 10; //Ou *(v + 2) = 10
+
+                //Passo 6 faça pz apontar para a primeira posicao do vetor
+                pz = v; //Ou pz = &v[0]
+
+                //Passo 7: Preencha as duas posições do vetor usando aritmética de ponteiros
+                *pz = b; //Primeira posição
+                *(pz + 1) = *px; //Segunda posição
+
+                //Passo 8: Imprima os conteúdos das variáveis e do vetor
+                cout << "a => " << a << endl;
+                cout << "b => " << b << endl;
+                cout << "c => " << c << endl;
+                cout << "Vetor => [";
+                for(int i = 0; i < 3; i++) {
+                    cout << v[i];
+                    if(i < 2)
+                        cout << ", ";
+                }
+                cout << "]" << endl;
+
+                //Passo 9: Libere a memoria alocada
+                delete [] v;
                 break;
             }
 
