e16d999f8faf40b94af65a042a699da0e63b3271
MarianaSiano <marianasianop@gmail.com>
2025-05-21 10:12:53 -0300

    Parte da aula 1 da teorica concluida

--- Teorica/aula01_ponteiros.cpp
+++ Teorica/aula01_ponteiros.cpp
@@ -5,24 +5,57 @@ using namespace std;
 
 int main()
 {
-    cout << "============= EXEMPLOS =============" << endl;
-    cout << "Exemplo 1" << endl;
-    cout << endl;
-    int vet[3] = {10, 20, 30};
+    int opcao;
 
-    cout << "Enderecos" << endl;
-    cout << vet << endl;
-    cout << vet + 1 << endl;
-    cout << vet + 2 << endl;
-    cout << endl;
+    do {
+        cout << "\n================= MENU =================" << endl;
+        cout << "1. Passo a Passo de Ponteiros" << endl;
+        cout << "0. Sair" << endl;
+        cout << "Escolha uma opcao => ";
+        cin >> opcao;
 
-    cout << "Valores" << endl;
-    cout << vet[0] << endl;
-    cout << vet[1] << endl;
-    cout << vet[2] << endl;
-    cout << endl;
+        switch(opcao) {
+            case 1: {
+                //Parte 1
+                int val;
+                int *ptr = nullptr;
+                int *ptr2 = nullptr;
 
-    cout << "============= ATIVIDADES =============" << endl;
-    cout << "Atividade 1" << endl;
-    cout << endl;
+                //Parte 2
+                cout << "Digite um valor INTEIRO => ";
+                cin >> val;
+                cout << "Variavel 'val' declarada com o valor => " << val << endl;
+
+                //Parte 3
+                ptr = &val;
+                cout << "'ptr' agora aponta para 'val'" << endl;
+
+                //Parte 4
+                cout << "Valor da variavel 'val' => " << val << endl;
+                cout << "Endereco da variavel 'val' => " << &val << endl;
+
+                //Parte 5
+                cout << "Endereço armazenado em ptr: " << ptr << endl;
+                cout << "Valor apontado por ptr: " << *ptr << endl;
+
+                //Parte 6
+                cout << "Digite o novo valor para variavel 'val' (via ptr) => ";
+                cin >> *ptr;
+                cout << "Valor da variavel 'val' modificado para => " << val << endl;
+
+                //Parte 7
+                cout << "Digite novo valor para o endereco apontado por 'ptr2' => ";
+                cin >> *ptr2;
+                cout << "Valor apontado por 'ptr2' modificado" << endl;
+
+                //Parte 8
+                cout << "Valor final de 'val' => " << val << endl;
+
+                break;
+            }
+        }
+
+    } while(opcao != 0);
+
+    return 0;
 }
