4f20be70c0296c92531d77030d21a186d4d6cd25
MarianaSiano <marianasianop@gmail.com>
2025-05-21 18:17:49 -0300

    Atividade 1 concluida

--- Teorica/aula02_recursividade.cpp
+++ Teorica/aula02_recursividade.cpp
@@ -3,53 +3,15 @@
 
 using namespace std;
 
-float *alocaVetor(int n)
+int somatorioRecursivo(int n)
 {
-    return new float[n];
-}
-
-//Funcao para Calcular a Media dos Elementos do Vetor
-float calcMedia(int n, float vet[])
-{
-    float soma;
-    soma = 0.0f;
-
-    for(int i = 0; i < n; i++)
-        soma+= vet[i];
-
-    return soma / n;
-}
-
-///Funcao que consta e imprime os elementos maiores que val
-int func1(int n, int vet[], int val)
-{
-    int count = 0;
-
-    for(int i = 0;  i < n; i++) {
-        if(vet[i] > val) {
-            cout << "Posicao " << i << " valor " << vet[i]
-            << " endereco " << &vet[i] << endl;
-        }
-    }
-
-    return count;
-}
-
-//Funçao que aloca e retorna um novo vetor com elementos maiores que val
-int *func2(int n, int vet[], int val, int tam)
-{
-    if(tam <= 0)
-        return nullptr; //Nenhum elemento maior
-
-    int *novoVetor = new int[tam];
-    int j = 0;
-
-    for(int i = 0; i < n; i++) {
-        if(vet[i] > val)
-            novoVetor[j++] = vet[i];
-    }
-
-    return novoVetor;
+    //Caso base: quando n é 1, retorna 1
+    if(n == 1)
+        return 1;
+    
+    //Passo recursivo: soma n com o somatório de n - 1
+    else
+        return n + (somatorioRecursivo(n - 1));
 }
 
 int main()
@@ -58,13 +20,24 @@ int main()
 
     do {
         cout << "\n================= MENU =================" << endl;
-        cout << "1. " << endl;
+        cout << "1. Somatorio" << endl;
         cout << "0. Sair" << endl;
         cout << "Escolha uma opcao => ";
         cin >> opcao;
 
         switch(opcao) {
             case 1: {
+                int numero;
+                cout << "Digite um numero inteiro positivo => ";
+                cin >> numero;
+
+                if(numero < 1) {
+                    cout << "O numero deve ser positivo!" << endl;
+                    return 1;
+                }
+
+                int resultado = somatorioRecursivo(numero);
+                cout << "O somatprio de 1 ate " << numero << " eh => " << resultado << endl;
                 break;
             }
 
