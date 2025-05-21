17cf9b55608bbdc17931bb52f4e0543b59a0dcf5
MarianaSiano <marianasianop@gmail.com>
2025-05-21 14:04:12 -0300

    Atividade 1 - Calcular media concluida

--- Teorica/aula01-parte02_alocacao.cpp
+++ Teorica/aula01-parte02_alocacao.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>  //Para usar fixed e setprecision
 using namespace std;
 
 int main()
@@ -7,13 +8,40 @@ int main()
 
     do {
         cout << "\n================= MENU =================" << endl;
-        cout << "1. " << endl;
+        cout << "1. Media dos Valores" << endl;
         cout << "0. Sair" << endl;
         cout << "Escolha uma opcao => ";
         cin >> opcao;
 
         switch(opcao) {
             case 1: {
+                int n;
+                float soma, media;
+                soma = media = 0.0f;
+
+                //Passo 1: Ler o numero de n de elementos
+                cout << "Digite o tamanho do vetor => ";
+                cin >> n;
+
+                //Verifica se n é valido
+                if(n <= 0) {
+                    cout << "Tamanho invalido! Vetir deve ter pelo menos 1 elemento." << endl;
+                    return 1;
+                }
+
+                //Passo 2: Alocar dinamicamente o vetor
+                float *vetor = new float(n);
+
+                //Passo 3: Calcular a media
+                media = soma / n;
+
+                //Passo 5: Imprimir a medica com duas casas decimais
+                cout << fixed << setprecision(2);
+                cout << "Media dos valores => " << media << endl;
+
+                //Passo 6: Liberar a memoria alocada
+                delete [] vetor;
+
                 break;
             }
 
