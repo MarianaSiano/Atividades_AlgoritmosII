1bbe75771f229ddd41dd24c75df291fa144df4e8
MarianaSiano <marianasianop@gmail.com>
2025-05-22 20:05:53 -0300

    Comecando a atividade 3

--- Pratica/aula05_recursividadeVetor.cpp
+++ Pratica/aula05_recursividadeVetor.cpp
@@ -86,6 +86,10 @@ int main()
                 break;
             }
 
+            case 3: {
+                break;
+            }
+
             case 0:
                 cout << "Encerrando o programa." << endl;
                 break;
